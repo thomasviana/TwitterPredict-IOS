d58e737d5376c0d6ada5e7c256d8fd762a599bac
Thomas Viana <thomasviana16@gmail.com>
2021-10-15 13:11:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TwitterPredict-IOS
--- TwitterPredict/ViewController.swift
+++ TwitterPredict/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func predictPressed(_ sender: Any) {
+    }
+    
 }
 
