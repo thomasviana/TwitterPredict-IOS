75f700a9368eaefa788b867d2157b8af5123f9fa
Thomas Viana <thomasviana16@gmail.com>
2021-10-16 10:26:00 -0500

    Updating the User Interface

--- TwitterPredict/ViewController.swift
+++ TwitterPredict/ViewController.swift
@@ -22,27 +22,66 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let prediction = try! sentimentClassifier.prediction(text: "@Apple is the best company")
         
-        print(prediction.label)
         
-        swifter.searchTweet(using: "@Apple",lang: "en", count: 100, tweetMode: .extended, success: { results, metadata in
-            var tweets = [String]()
-            
-            for i in 0..<100 {
-                if let tweet = results[i]["full_text"].string {
-                    tweets.append(tweet)
-                }
-            }
-            
-            
-        }) { error in
-            print("There wa an error with the Twitter API Request")
-        }
+        
         
     }
 
     @IBAction func predictPressed(_ sender: Any) {
+        if let searchText = textField.text {
+            swifter.searchTweet(using: searchText ,lang: "en", count: 100, tweetMode: .extended, success: { results, metadata in
+                var tweets = [TweetSentimentClassifierInput]()
+                
+                for i in 0..<100 {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                        tweets.append(tweetForClassification)
+                    }
+                }
+                
+                do {
+                    let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                    
+                    var sentimentScore = 0
+                    
+                    for pred in predictions {
+                        let sentiment = pred.label
+                        if sentiment == "Pos" {
+                            sentimentScore += 1
+                            
+                        } else if sentiment == "Neg" {
+                            sentimentScore -= 1
+                        }
+                    }
+                    
+                    if sentimentScore > 20 {
+                        self.sentimentLabel.text = "🤩"
+                    } else if sentimentScore > 10 {
+                        self.sentimentLabel.text = "😄"
+                    } else if sentimentScore > 0 {
+                        self.sentimentLabel.text = "🙂"
+                    } else if sentimentScore == 0 {
+                        self.sentimentLabel.text = "😐"
+                    } else if sentimentScore > -10 {
+                        self.sentimentLabel.text = "😕"
+                    } else if sentimentScore > -20 {
+                        self.sentimentLabel.text = "😡"
+                    } else {
+                        self.sentimentLabel.text = "🤮"
+                    }
+                } catch {
+                    print(error)
+                }
+                
+            }) { error in
+                print("There wa an error with the Twitter API Request")
+            }
+        }
+        
+        let prediction = try! sentimentClassifier.prediction(text: "@Apple is the best company")
+        
+        print(prediction.label)
     }
     
 }
